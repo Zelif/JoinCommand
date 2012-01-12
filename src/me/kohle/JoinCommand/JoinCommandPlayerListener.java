18cb6fd5c2c3ae5578d713067ea6fc3e3aa8464c
Zelif <jarodk.1988@gmail.com>
2012-01-12 19:19:52 +1100

    Cleaned up code and made the get list code easier and not duplicated like before.

--- src/me/kohle/JoinCommand/JoinCommandPlayerListener.java
+++ src/me/kohle/JoinCommand/JoinCommandPlayerListener.java
@@ -3,12 +3,14 @@ package me.kohle.JoinCommand;
 
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerListener;
 
 
 
+@SuppressWarnings("unused")
 public class JoinCommandPlayerListener extends PlayerListener {
 
 	public JoinCommand plugin;
@@ -19,38 +21,36 @@ public class JoinCommandPlayerListener extends PlayerListener {
 	public JoinCommandPlayerListener(JoinCommand instance) {
 		plugin = instance;
 	}
-	public void hasPlayedBefore(){
-		
+	
+	public void filterCommand(Player player , String listType){
+		List<Object> stuff = plugin.getConfig().getList(listType);
+        if(!(stuff == null)){
+        	for (Object s : stuff) {
+        		player.performCommand((String) s); 
+        		//Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), (String) s);
+        		//Thinking about making this command done by the server not the player as new people might not have any permissions.
+        		//Only for first join tho
+        	}
+        }
 	}
 	
-	
-	@SuppressWarnings("unused")
 	@Override
 	public void onPlayerJoin(PlayerJoinEvent event) {
 		Player player = event.getPlayer();
         String name = player.getName();
         
-        if(!player.hasPlayedBefore()){
-            //Call to get FirstCommands if the person has no dat file (first time logging on)
-            List<Object> stuff2 = plugin.getConfig().getList("FirstCommand");
-    	if(!(stuff2 == null)){
-        for (Object s : stuff2) {
-        	player.performCommand((String) s); 
-        	System.out
-        	.println( "[JoinCommand] " + name + ": logged in for first time. " );
-        	}
-        }
-        }
-
-		//If the player is not present when the player joins:
-		if(player == null) {
+		if(player == null) { 	//If the player is not present when the player joins
 			System.out.println("[JoinCommand] A non-player joined the server... what would do that?!");
 		}
-		List<Object> stuff = plugin.getConfig().getList("Commands");
-		if(!(stuff == null)){
-			for (Object s  : stuff) {
-				player.performCommand((String) s);
-			}
+
+        if(!player.hasPlayedBefore()){
+        	String listType = "FirstComamnd";   //Call to get FirstCommands if its the players first time logging on
+            filterCommand(player , listType);
+        	System.out
+        	.println( "[JoinCommand] " + name + ": logged in for first time. " );
+        }
+
+    	String listType = "Commands";
+        filterCommand(player , listType);
 		}
-	}
 }
