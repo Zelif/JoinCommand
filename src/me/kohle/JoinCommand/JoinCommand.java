0bd1f30c9dfab9a122a273b0e8584a45d70573ac
Zelif <jarodk.1988@gmail.com>
2012-01-15 11:54:02 +1100

    Updated to latest, added jc-addcommand(also has jc-ac as I got sick of typing it a lot in testing)

--- src/me/kohle/JoinCommand/JoinCommand.java
+++ src/me/kohle/JoinCommand/JoinCommand.java
@@ -1,5 +1,6 @@
 package me.kohle.JoinCommand;
 import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.bukkit.command.Command;
@@ -7,20 +8,21 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event.Priority;
 import org.bukkit.event.Event.Type;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.configuration.file.FileConfiguration;
 
 public class JoinCommand extends JavaPlugin {
-	
+
 	
 	//Setting default list
-	String[] list = {"hi", "version"};
+	String[] list = {"jc", "version"}; //Changed all "hi" to jc hopefully no other plugin uses jc
 
 
 	public void loadConfiguration(){
 
-		//Cut out having to use this.getConfig all the time final makes sure it doesn't get changed
+		//Cut out having to use this.getConfig all the time final makes sure it doesn't get changed.
 		final FileConfiguration config = this.getConfig();
 		config.addDefault("Commands", Arrays.asList(list));
 		config.addDefault("FirstCommand", Arrays.asList(list));
@@ -35,22 +37,40 @@ public class JoinCommand extends JavaPlugin {
 	Logger log = Logger.getLogger("Minecraft");
 	
 
-	  public void onEnable() {
+	public void onEnable() {
 		  
-		  loadConfiguration();
+		PluginDescriptionFile pdfFile = this.getDescription();
+		System.out.println("[" + pdfFile.getName() + "] Version" + pdfFile.getVersion() + " by" + pdfFile.getAuthors() + " enabled!");
 		  
-		  pm = getServer().getPluginManager();
-		  pluginInfo("enabled, with a configuration file!");
-		  pm.registerEvent(Type.PLAYER_JOIN, playerListener, Priority.High, this);
+		loadConfiguration();
+		  
+		pm = getServer().getPluginManager();
+		pm.registerEvent(Type.PLAYER_JOIN, playerListener, Priority.High, this);
 	  }
 	  public void onDisable() {
-	    pluginInfo("disabled.");
-	    reloadConfig(); // Now reloads the config when disabled (/reload can be used now and not a server restart)
+		  PluginDescriptionFile pdfFile = this.getDescription();
+		  System.out.println("[" + pdfFile.getName() + "] Version" + pdfFile.getVersion() + " disabled.");
+		  reloadConfig(); // Now reloads the config when disabled (/reload can be used now and not a server restart)
 	    //The save on disable that was here prevented writing to the file when the server was active.
 	  } 
-	  public static void pluginInfo(String message) {
-	    	String v = "1.1-b";
-	    	System.out.println("[JoinCommand] Version " + v + " " + message);
+	  
+	  private void addCommand(String[] args , CommandSender sender) {
+		  String listType = args[0]; //this gets the list name
+		  List<Object> loadedList = this.getConfig().getList(listType);
+		  if(!(loadedList == null)){ //checks config to see if it exists
+			  String s = "";
+			  for(int i = 1; i < args.length; i++){
+				  s = s + args[i] + " ";
+			  }
+			  loadedList.add((Object) s);
+			  this.getConfig().addDefault(listType, loadedList); 
+			  sender.sendMessage("Command " + (String) s + "added to " + args[0]);
+			  saveConfig();
+		  }
+		  else{
+			  sender.sendMessage("Incorrect list name, enter either Commands or FirstCommand");
+		  }
+
 	  }
 	  
 	  public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
@@ -58,21 +78,26 @@ public class JoinCommand extends JavaPlugin {
 		  if (sender instanceof Player) {	// Sets the sender if it is a player
 			player = (Player) sender;
 		  }
-		  	if(commandLabel.equalsIgnoreCase("hi")) {
+		  	if(commandLabel.equalsIgnoreCase("jc")) {
 		  		if(player == null) {  //if there is no player it will issue a msg in the console
 		  			sender.sendMessage("[JoinCommand] Only players can issue this command.");
 		  		}
 		  		else{	//if it has been sent by a player the default msgs are used.
 		  			player.sendMessage("Please Edit the Config file, replace");
-		  			player.sendMessage("hi and hi2 with the command(s) you want");
+		  			player.sendMessage("'jc' and 'version' with the command(s) you want!");
 		  		}
 				return true;
 			}
 		  else if(commandLabel.equalsIgnoreCase("jc-reload") && sender.hasPermission("JoinCommand.reload")) {   //ingame command to reload config, can also be used in console
 			  reloadConfig();
-			  sender.sendMessage("JoinCommand config reloaded.");
+			  sender.sendMessage("JoinCommand configuration file reloaded.");
+			  return true;
+		  }
+		  else if((commandLabel.equalsIgnoreCase("jc-addcommand") || commandLabel.equalsIgnoreCase("jc-ac")) && sender.hasPermission("JoinCommand.addcommand")) {   //ingame command to add items to config, can also be used in console  
+			  addCommand(args , sender);
 			  return true;
 		  }
 		return false;
 	  }
+
 }
