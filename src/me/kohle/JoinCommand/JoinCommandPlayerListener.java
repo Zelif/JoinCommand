7aa614a3b5070541a23f61f6560c4b7b554fee86
Zelif <jarodk.1988@gmail.com>
2012-03-02 04:59:43 +1100

    Updated to R6 but config does not work Sleep first

--- src/me/kohle/JoinCommand/JoinCommandPlayerListener.java
+++ src/me/kohle/JoinCommand/JoinCommandPlayerListener.java
@@ -1,57 +1,42 @@
 package me.kohle.JoinCommand;
 
-
 import java.util.List;
 
-import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerListener;
 
-
-
-@SuppressWarnings("unused")
-public class JoinCommandPlayerListener extends PlayerListener {
-
+public class JoinCommandPlayerListener implements Listener {
 	public JoinCommand plugin;
-  	
-
-    
-
-	public JoinCommandPlayerListener(JoinCommand instance) {
-		plugin = instance;
+	public JoinCommandPlayerListener(JoinCommand plugin) {
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
 	}
 	
 	public void filterCommand(Player player , String listType){
-		List<Object> stuff = plugin.getConfig().getList(listType);
-        if(!(stuff == null)){
-        	for (Object s : stuff) {
-        		player.performCommand((String) s); 
-        		//Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), (String) s);
+		FileConfiguration config = plugin.getConfig();
+		List<String> stuff = config.getStringList(listType); //Doesn't read config so throws a null
+       		if(!(stuff == null)){
+        		for (String s : stuff) {
+        			player.performCommand((String) s); 
+        		}
         	}
-        }
 	}
-	
-	@Override
+    @EventHandler(priority = EventPriority.HIGH)	
 	public void onPlayerJoin(PlayerJoinEvent event) {
 		Player player = event.getPlayer();
-        String name = player.getName();
-        
-		if(player == null) { 	//If the player is not present when the player joins
-			System.out.println("[JoinCommand] A non-player joined the server... what would do that?!");
-		}
-
-        if(!player.hasPlayedBefore() && player.hasPermission("JoinCommand.FirstCommand")) {
-
-        	String listType = "FirstCommand";   //Call to get FirstCommands if its the players first time logging on
-            filterCommand(player , listType);
-        	System.out
-        	.println( "[JoinCommand] " + name + ": logged in for first time. " );
-        }
-        if(player.hasPermission("JoinCommand.NormalCommand")) {
-    		String listType = "Commands";
-    		filterCommand(player , listType);
-        }
+        	String name = player.getName();
+        	if(!player.hasPlayedBefore() && player.hasPermission("JoinCommand.FirstCommand")) {
+        		String listType = "FirstCommand";   //Call to get FirstCommands if its the players first time logging on
+            		filterCommand(player , listType);
+        		System.out.println( "[JoinCommand] The player " + name + " logged in for first time. " );
+        	}
+        	if(player.hasPermission("JoinCommand.NormalCommand")) {
+    			String listType = "Commands";
+    			filterCommand(player , listType);
+        	}
 
 	}
 }
