1bd476228e588162129be5a76d245ab84e9f36a2
Zelif <jarodk.1988@gmail.com>
2012-03-04 18:25:00 +1100

    Fixed for R6

--- src/me/kohle/JoinCommand/JoinCommand.java
+++ src/me/kohle/JoinCommand/JoinCommand.java
@@ -6,6 +6,7 @@ import java.util.logging.Logger;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.configuration.file.FileConfiguration;
 
@@ -24,7 +25,9 @@ public class JoinCommand extends JavaPlugin {
 	Logger log = Logger.getLogger("Minecraft");
 	public void onEnable() {
 		loadConfiguration();
-		new JoinCommandPlayerListener(this);
+		//new JoinCommandPlayerListener(this);
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(new JoinCommandPlayerListener(this), this);
 		
 	  }
 	  public void onDisable() {
