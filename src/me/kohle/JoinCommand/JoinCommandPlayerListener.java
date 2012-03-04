ba0887b3b33b47ef490b37d032a078707a18461b
Zelif <jarodk.1988@gmail.com>
2012-03-04 18:28:08 +1100

    Fixed for R6

--- src/me/kohle/JoinCommand/JoinCommandPlayerListener.java
+++ src/me/kohle/JoinCommand/JoinCommandPlayerListener.java
@@ -2,7 +2,6 @@ package me.kohle.JoinCommand;
 
 import java.util.List;
 
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -10,27 +9,26 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class JoinCommandPlayerListener implements Listener {
-	public JoinCommand plugin;
-	public JoinCommandPlayerListener(JoinCommand plugin) {
-        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+	JoinCommand plugin;
+	public JoinCommandPlayerListener(JoinCommand instance) {
+        	plugin = instance;
 	}
 	
 	public void filterCommand(Player player , String listType){
-		FileConfiguration config = plugin.getConfig();
-		List<String> stuff = config.getStringList(listType); //Doesn't read config so throws a null
-       		if(!(stuff == null)){
+		List<String> stuff = plugin.getConfig().getStringList(listType);
+        	if(!(stuff == null)){
         		for (String s : stuff) {
         			player.performCommand((String) s); 
         		}
         	}
 	}
-    @EventHandler(priority = EventPriority.HIGH)	
+    	@EventHandler(priority = EventPriority.HIGH)	
 	public void onPlayerJoin(PlayerJoinEvent event) {
 		Player player = event.getPlayer();
         	String name = player.getName();
         	if(!player.hasPlayedBefore() && player.hasPermission("JoinCommand.FirstCommand")) {
         		String listType = "FirstCommand";   //Call to get FirstCommands if its the players first time logging on
-            		filterCommand(player , listType);
+        		filterCommand(player , listType);
         		System.out.println( "[JoinCommand] The player " + name + " logged in for first time. " );
         	}
         	if(player.hasPermission("JoinCommand.NormalCommand")) {
